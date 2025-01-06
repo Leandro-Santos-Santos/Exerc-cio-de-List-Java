886a2c6449f8e1b557c08b32f45e4129c3b4f4fd
Leandro-Santos-Santos <santosleandro1805@gmail.com>
2025-01-06 06:06:53 -0300

    Terminando meu codigo com pacote de project

---
+++ br.com.dio.collection.list/src/exercicio1.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+//Faça um programa que receba a temperatura média dos 6
+//primeiros meses do ano e armazene-as em uma lista.
+
+//Após isto, calcule a média semestral das temperaturas e
+//mostre todas as temperaturas acima desta média, e em que
+//mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2
+//– Fevereiro e etc).
+
+
+
+class exercicio1 {
+    public static void main(String[] args) {
+        //Scanner scanner = new Scanner(System.in);
+        String[] mes = {"Janeiro", "Fevereiro","Março","Abril","Maio","Junho"};
+
+        List<Double> temperaturas = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i = 0; i < mes.length; i++) {
+            System.out.print("Digite a temperatura média de " + mes[i] + ": ");
+            double temperatura = scanner.nextDouble();
+            temperaturas.add(temperatura);
+        }
+
+    double soma = 0;
+    for (double temp : temperaturas){
+        soma += temp;
+    }    
+    
+    double mediaSemestral = soma / temperaturas.size();    
+    System.out.println("\nA média semestral das temperaturas é: " + mediaSemestral);
+
+    System.out.println("\nMeses com temperatura acima da mádia semestral:");
+    for (int i = 0; i < temperaturas.size(); i++) {
+        if(temperaturas.get(i) > mediaSemestral) {
+            System.out.println(mes[i] + ": " + temperaturas.get(i));
+        }
+    }
+
+    scanner.close();
+    }
+
+    
+}
